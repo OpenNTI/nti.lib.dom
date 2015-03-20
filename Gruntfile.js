1990e50cbc1ac7c9f808e306a9ef41c0160f841c
Jonathan Grimes <jonathan.grimes@nextthought.com>
2015-03-20 10:43:13 -0500

    initial commit.

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = function(grunt) {
+	// Let *load-grunt-tasks* require everything
+	require('load-grunt-tasks')(grunt);
+
+	grunt.initConfig({
+
+		karma: {
+            unit: {
+                configFile: 'karma.conf.js'
+            }
+        },
+
+
+		jshint: {
+	        options: {
+				jshintrc: true,
+	            reporter: require('jshint-log-reporter')
+			},
+	        files: [
+				'**/*.js'
+			]
+	    }
+	});
+
+	//grunt.registerTask('docs',['jsdoc']);
+	grunt.registerTask('test', ['karma']);
+	grunt.registerTask('default', ['jshint']);
+
+};
