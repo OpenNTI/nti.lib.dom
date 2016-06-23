c6338e96b5b6d344a2ec89f58c087bd876f33346
Jonathan Grimes <jonathan.grimes@nextthought.com>
2016-06-23 17:12:37 -0500

    only check elements

--- src/get-effective-z-index.js
+++ src/get-effective-z-index.js
@@ -1,7 +1,9 @@
+import isElement from './iselement';
+
 export default function getEffectiveZIndex (el) {
 	let zindex = 0;
 
-	while (el) {
+	while (el && isElement(el)) {
 		const style = getComputedStyle(el);
 		const z = style && parseInt(style.getPropertyValue('z-index'), 10);
 
