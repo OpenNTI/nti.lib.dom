d1015870f645490e57c57555fd01793d7abd8b4a
Jonathan Grimes <jonathan.grimes@nextthought.com>
2017-03-30 17:37:37 -0500

    toCSSClass: add camelCase => camel-case conversion

--- src/tocssclassname.js
+++ src/tocssclassname.js
@@ -1,5 +1,7 @@
 export default function toCSSClass (txt) {
-	return txt.toLowerCase()
+	return txt
+		.replace(/([a-z])([A-Z])/g, '$1-$2') //split cammelCase to cammel-case
+		.toLowerCase()
 			.replace(/[^a-z0-9]/ig, '-')
 			.replace(/-+/g, '-')
 			.replace(/-$/, '')
