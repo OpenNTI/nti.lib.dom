9384a7b88f11b1a141c3c8be4a1bab90117421ca
Jonathan Grimes <jonathan.grimes@nextthought.com>
2017-04-24 12:23:39 -0500

    also get the width/height of scroll in result

--- src/getscrollposition.js
+++ src/getscrollposition.js
@@ -4,6 +4,8 @@ import getStyle from './getstyle';
 const getDocEl = (win) => win.document && win.document.documentElement || document.body;
 const getWindowScrollTop = (win) => getDocEl(win).scrollTop;
 const getWindowScrollLeft = (win) => getDocEl(win).scrollLeft;
+const getWindowScrollHeight = (win) => getDocEl(win).scrollHeight;
+const getWindowScrollWidth = (win) => getDocEl(win).scrollWidth;
 
 export default function getScrollPosition (el) {
 	const overflow = /auto|scroll/i;
@@ -18,5 +20,8 @@ export default function getScrollPosition (el) {
 	return {
 		top: el.scrollTop || el.scrollY || getWindowScrollTop(el),
 		left: el.scrollLeft || el.scrollX || getWindowScrollLeft(el),
+
+		height: el.scrollHeight || getWindowScrollHeight(el),
+		width: el.scrollWidth || getWindowScrollWidth(el),
 	};
 }
