330bb8880b3fd735c68bf3a38b9ab0bfa5c3809b
Ray Hatfield <ray.hatfield@nextthought.com>
2017-04-27 17:26:38 -0500

    typo in comment

--- src/tocssclassname.js
+++ src/tocssclassname.js
@@ -1,6 +1,6 @@
 export default function toCSSClass (txt) {
 	return txt
-		.replace(/([a-z])([A-Z])/g, '$1-$2') //split cammelCase to cammel-case
+		.replace(/([a-z])([A-Z])/g, '$1-$2') //split camelCase to camel-case
 		.toLowerCase()
 			.replace(/[^a-z0-9]/ig, '-')
 			.replace(/-+/g, '-')
