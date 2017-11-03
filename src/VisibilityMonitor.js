b3abcb5e12540749a23d8281a5c967bd0179218a
Jonathan Grimes <jsg2021@gmail.com>
2017-11-03 11:20:52 -0500

    update VisibilityMonitor... add a way to stop listening

--- src/VisibilityMonitor.js
+++ src/VisibilityMonitor.js
@@ -1,68 +1,66 @@
 import EventEmitter from 'events'; //builtin
 
-const CHANGE_EVENT = 'visibilitychange';
-let views = 0;
-let prefix = (()=> {
-
+const prefix = (()=> {
 	try {
-		let prefixes = ['webkit', 'moz', 'ms', 'o'];
-		let p = null, i = 0;
-		if (typeof document === 'undefined') {
-			return;
-		}
-
+		const prefixes = ['webkit', 'moz', 'ms', 'o'];
 		if (document.hidden !== undefined) {
-			p = '';
+			return '';
 		}
-		else {
-			// Test all vendor prefixes
-			for(; i < prefixes.length; i++) {
-				if (document[prefixes[i] + 'Hidden'] !== undefined) {
-					p = prefixes[i];
-					break;
-				}
+		// Test all vendor prefixes
+		for(let i = 0; i < prefixes.length; i++) {
+			if (document[prefixes[i] + 'Hidden'] !== undefined) {
+				return prefixes[i];
 			}
 		}
-
-		return p;
-	} catch(e) {
-		return null;
 	}
-
+	catch(e) {/**/}
+	return null;
 })();
 
+const CHANGE_EVENT = 'visibilitychange';
 const eventName = prefix + 'visibilitychange';
 const propertyName = prefix === '' ? 'hidden' : (prefix + 'Hidden');
 
+
 class VisibilityMonitor extends EventEmitter {
 
-	getViews () { return views; }
+	constructor () {
+		super();
+
+		const doc = typeof document !== 'undefined' ? document : null;
+
+		if (prefix !== null && doc) {
+
+			const handleVisibilityChange = () => {
+				const hidden = doc[propertyName];
+
+				// The page is in foreground and visible
+				if (hidden === false) {
+					this.views++;
+				}
+				//console.debug('Emitting: Visibility Change: hidden? ', hidden);
+				this.emit(CHANGE_EVENT, !hidden);
+			};
+
+			doc.addEventListener(eventName, handleVisibilityChange);
+			handleVisibilityChange();
+		}
+	}
+
+
+	getViews () { return this.views; }
+
 
 	addChangeListener (callback) {
-		this.on(CHANGE_EVENT, callback);
+		this.addEventListener(CHANGE_EVENT, callback);
+		const unsubscribe = () => this.removeEventListener(CHANGE_EVENT, callback);
+		return unsubscribe;
+	}
+
+
+	removeChangeListener (callback) {
+		this.removeEventListener(callback);
 	}
 }
 
-let mon = new VisibilityMonitor();
-
-function countView () {
-	let hidden = document[propertyName];
-
-	// The page is in foreground and visible
-	if (hidden === false) {
-		views++;
-	}
-	//console.debug('Emitting: Visibility Change: hidden? ', hidden);
-	mon.emit(CHANGE_EVENT, !hidden);
-}
-
-function setupPageVisibility () {
-	if (prefix !== null && typeof document !== 'undefined') {
-		document.addEventListener(eventName, countView);
-		countView();
-	}
-}
-
-setupPageVisibility();
-
-export default mon;
+export default new VisibilityMonitor();
