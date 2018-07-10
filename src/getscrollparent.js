dd7115d82d2b220e4719e9a29eed9eb767c7b70b
Jonathan Grimes <jsg2021@gmail.com>
2018-07-10 11:26:14 -0500

    preserve our expected behavior in tests

--- src/getscrollparent.js
+++ src/getscrollparent.js
@@ -2,4 +2,9 @@ import scrollParent from 'scrollparent';
 
 const WINDOW = typeof window === 'undefined' ? global : window;
 const shouldBeWindow = el => !el || el.tagName === 'BODY' && el.clientHeight <= el.scrollHeight;
+
+if (typeof document !== 'undefined' && !document.scrollingElement) {
+	document.scrollingElement = WINDOW;
+}
+
 export default el => (x => shouldBeWindow(x) ? WINDOW : x)(scrollParent(el));
