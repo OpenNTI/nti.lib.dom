27634a706c6a99fb40cf7e77353d0716c94c8a5b
Andrew Ligon <andrew.ligon@nextthought.com>
2020-03-03 12:54:20 -0600

    add more utility methods for focusing elements

--- src/nodes/focus-element.js
+++ src/nodes/focus-element.js
@@ -1,5 +1,42 @@
+import {matches} from './matches';
+
+//Taken from: https://gomakethings.com/how-to-get-the-first-and-last-focusable-elements-in-the-dom/
+const focusableSelector = 'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])';
+
 export function focusElement (el) {
 	if (el && el.focus) {
 		el.focus();
+	}
+}
+
+export function getFocusableDescendant (el) {
+	if (el?.querySelectorAll) {
+		return el.querySelectorAll(focusableSelector);
+	}
+
+	return [];
+}
+
+export function isFocusable (el) {
+	return matches(el, focusableSelector);
+}
+
+export function focusDescendant (el) {
+	const descendants = Array.from(getFocusableDescendant(el));
+	const first = descendants ? descendants[0] : null;
+
+	if (first) {
+		focusElement(first);
+	}
+}
+
+export function focusDescendantOrElement (el) {
+	const descendants = Array.from(getFocusableDescendant(el));
+	const first = descendants ? descendants[0] : null;
+
+	if (first) {
+		focusElement(first);
+	} else if (isFocusable(el)) {
+		focusElement(el);
 	}
 }
