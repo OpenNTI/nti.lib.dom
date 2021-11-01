d6a048150fa02c473a0047729fb118a2607f8483
Jonathan Grimes <jsg2021@gmail.com>
2021-11-01 16:40:19 -0500

    add type

--- src/layout/get-element-rect.js
+++ src/layout/get-element-rect.js
@@ -2,20 +2,28 @@ import { isElement } from '../nodes/is-element';
 
 import { getViewportWidth, getViewportHeight } from './viewport';
 
+/**
+ * @typedef {object} Rect
+ * @property {number} top
+ * @property {number} left
+ * @property {number} right
+ * @property {number} bottom
+ * @property {number} width
+ * @property {number} height
+ */
+
+/**
+ * @param {HTMLElement} el
+ * @returns {Rect}
+ */
 export function getElementRect(el) {
 	let rect, w, h;
 	if (el && el.getBoundingClientRect) {
 		//getBoundingClientRect returns a ClientRect which is not a "normal" JSO.
 		//So Object operations or mutations do not work on it. Pull the values
 		//out and returna JSO.
-		const {
-			top,
-			left,
-			bottom,
-			right,
-			width,
-			height,
-		} = el.getBoundingClientRect();
+		const { top, left, bottom, right, width, height } =
+			el.getBoundingClientRect();
 		rect = { top, left, bottom, right, width, height };
 	}
 
